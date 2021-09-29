0e5bd98987abc4d907a7aac54d327d02ef202f99
Eldar Farkhutdinov <91593704+EldarFarkhutdinov@users.noreply.github.com>
2021-09-29 17:30:08 +0300

    Update Configuration.h

--- BTT E3 RRF V1.1/Firmware/Marlin/Ender3/Marlin-2.0.7.2.x-E3-RRF/Marlin/Configuration.h
+++ BTT E3 RRF V1.1/Firmware/Marlin/Ender3/Marlin-2.0.7.2.x-E3-RRF/Marlin/Configuration.h
@@ -131,7 +131,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender-3 Eldar"
+#define CUSTOM_MACHINE_NAME "Ender-3 Eldar" ///+-EL 29.09.2021
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
